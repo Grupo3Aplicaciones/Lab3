8402c610ccc8a4365d881894038fc6b08f51a625
yoston <yostoncornet@hotmail.es>
2020-11-23 18:15:30 -0500

    actividad 5

--- app/src/main/java/com/example/lab3/PerfilUsuario.java
+++ app/src/main/java/com/example/lab3/PerfilUsuario.java
@@ -8,14 +8,21 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 import java.util.HashMap;
+import java.util.Map;
 
 public class PerfilUsuario extends AppCompatActivity {
 
     TextView txt_id, txt_name, txt_email;
     ImageView imv_photo;
     Button btn_logout;
+    DatabaseReference db_reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +42,10 @@ public class PerfilUsuario extends AppCompatActivity {
         txt_email.setText(info_user.get("user_email"));
         String photo = info_user.get("user_photo");
         Picasso.with(getApplicationContext()).load(photo).into(imv_photo);
+
+        iniciarBaseDeDatos();
+        leerTweets();
+        escribirTweets(info_user.get("user_name"));
     }
 
     public void cerrarSesion(View view){
@@ -44,4 +55,36 @@ public class PerfilUsuario extends AppCompatActivity {
         intent.putExtra("msg", "cerrarSesion");
         startActivity(intent);
     }
+
+    public void iniciarBaseDeDatos(){
+        db_reference = FirebaseDatabase.getInstance().getReference().child("Grupo");
+    }
+    public void leerTweets(){
+        db_reference.child("Grupo 3").child("tweets")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            System.out.println(snapshot);
+                        }
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+                        System.out.println(error.toException());
+                    }
+                });
+    }
+    public void escribirTweets(String autor){
+        String tweet = "hola mundo firebase 2";
+        String fecha = "31/10/2019";
+        Map<String, String> hola_tweet = new HashMap<String, String>();
+        hola_tweet.put("autor", autor);
+        hola_tweet.put("fecha", fecha);
+        DatabaseReference tweets = db_reference.child("Grupo 3").child("tweets");
+        tweets.setValue(tweet);
+        tweets.child(tweet).child("autor").setValue(autor);
+        tweets.child(tweet).child("fecha").setValue(fecha);
+    }
+
+
 }
